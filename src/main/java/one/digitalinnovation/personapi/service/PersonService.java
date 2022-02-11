0a0ea0b5bfe7030a1226025a0b45d3857cef6c0f
Rommel <robefo@gmail.com>
2022-02-11 19:11:22 -0300

    Developed person create operation.

---
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -0,0 +1,23 @@
+package one.digitalinnovation.personapi.service;
+
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public Person createPerson(@RequestBody Person person){
+        return personRepository.save(person);
+
+    }
+}
